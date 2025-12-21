dc794a205b403b4e7bd7f2dd0336399d61deca82
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2025-12-21 01:05:22 -0300

    FEAT(Service): Implement UserService with BCrypt encryption and SecurityConfig

---
+++ src/main/java/com/taskmanager/service/UserService.java
@@ -0,0 +1,40 @@
+package com.taskmanager.service;
+
+import com.taskmanager.entity.User;
+import com.taskmanager.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User save(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+        User userToSave = User.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .build();
+        return userRepository.save(userToSave);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+}
